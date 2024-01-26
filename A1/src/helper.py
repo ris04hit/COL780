62f7e5afdcf4c5a83b8919c6e3c0c9271fea4446
ris04hit <ris04hit@gmail.com>
2024-01-26 15:55:23 +0000

    Implemented canny edge detection

---
+++ A1/src/helper.py
@@ -0,0 +1,110 @@
+import numpy as np
+import scipy.signal
+
+# Function to convert image to gray scale image
+def gray_scale(img):
+    return 0.299*img[:,:,0] + 0.587*img[:,:,1] + 0.114*img[:,:,2]
+
+# Function to create a gaussian filter matrix of desired dimension
+def gaussian_filter_kernel(n):
+    k = (n-1)//2
+    filter_exp = np.array([[(i-k)**2 + (j-k)**2 for j in range(2*k+1)] for i in range(2*k+1)])
+    filter_val = np.exp(-filter_exp/2)
+    return filter_val/np.sum(filter_val)
+
+# Function to apply a filter kernel
+def filter_img(img, kernel):
+    if len(img.shape) == 3:
+        filtered_channel = []
+        for channel in range(img.shape[2]):
+            filtered_channel.append(scipy.signal.convolve2d(img[:, :, channel], kernel))
+        filtered_img = np.stack(filtered_channel, axis=-1)
+        return filtered_img
+    else:
+        return scipy.signal.convolve2d(img, kernel)
+
+# Changes the contrast by given value 
+def apply_contrast(img, c):
+    f = (259*(c+255))/(255*(259-c))
+    contrast_img = f*(img-128)+128
+    contrast_img[contrast_img < 0] = 0
+    contrast_img[contrast_img > 255] = 255
+    return contrast_img
+
+# Thresholds image
+def threshold(img, t):
+    return 255*(img > t)
+
+# Preprocessed image
+def preprocess(img, smooth_size = 5, contrast = 200):
+    # Gray scale
+    gray_img = gray_scale(np.copy(img))
+    
+    # Gaussian smoothening
+    kernel = gaussian_filter_kernel(smooth_size)
+    smoothened_img = filter_img(gray_img, kernel)
+    
+    # Increasing Contrast
+    high_contrast_img = apply_contrast(smoothened_img, contrast)
+    
+    # Thresholding
+    threshold_img = threshold(high_contrast_img, 50)
+    
+    return threshold_img
+
+
+# Calculates sobel kernel
+def sobel_gradient_kernel():
+    return np.array([[[-1, 0, 1],
+                      [-2, 0, 2],
+                      [-1, 0, 1]],
+                     [[1, 2, 1],
+                      [0, 0, 0],
+                      [-1, -2, -1]]])
+
+# Gradient thresholding
+def grad_threshold(grad, theta):
+    threshold = np.zeros_like(grad)
+    theta = np.round(theta/45).astype(int)%4
+    
+    for i in range(1, grad.shape[0]-1):
+        for j in range(1, grad.shape[1]-1):
+            if theta[i, j] == 0:
+                neighbor_val = max(theta[i, j-1], theta[i, j+1])
+            elif theta[i, j] == 1:
+                neighbor_val = max(theta[i-1, j+1], theta[i+1, j-1])
+            elif theta[i, j] == 2:
+                neighbor_val = max(theta[i-1, j], theta[i+1, j])
+            else:
+                neighbor_val = max(theta[i-1, j-1], theta[i+1, j+1])
+                
+            threshold[i, j] = grad[i,j] if grad[i, j] > neighbor_val else 0
+
+    return threshold
+
+# Double threshold
+def double_threshold(img, t1, t2):
+    img[img < t1] = 0
+    img[img > t2] = 255
+    return img
+
+# Canny Edge Detector
+def canny_edge_detector(img, threshold1 = 50, threshold2 = 200):
+    img = np.copy(img)
+    
+    # Calculates gradient
+    sobel_kernel = sobel_gradient_kernel()
+    grad_x = filter_img(img, sobel_kernel[0])
+    grad_y = filter_img(img, sobel_kernel[1])
+    grad = np.hypot(grad_x, grad_y)
+    theta = 180*np.arctan2(grad_y, grad_x)/np.pi
+    
+    # Gradient thresholding
+    threshold_img = grad_threshold(grad, theta)
+    threshold_img = 255*threshold_img/np.max(threshold_img)
+    
+    # Double threshold
+    d_threshold_img = double_threshold(threshold_img, threshold1, threshold2)
+
+    return d_threshold_img
+
---
+++ A1/src/main.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import cv2
+import numpy as np
+import helper
+
+if __name__ == '__main__':
+    part_id = sys.argv[1]
+    img_dir = sys.argv[2]
+    output_csv = sys.argv[3]
+    
+    # Part 1
+    if part_id == '1':
+        img_name_arr = os.listdir(img_dir)
+        img_name_arr.sort()
+        img_arr = [cv2.imread(os.path.join(img_dir, img_name)) for img_name in img_name_arr]
+
+        ct = 0
+        for img in img_arr:
+            processed_img = helper.preprocess(img)
+            edge_detected_img = helper.canny_edge_detector(processed_img)
+            cv2.imwrite(os.path.join('temp/', img_name_arr[ct]), edge_detected_img)
+            ct += 1
